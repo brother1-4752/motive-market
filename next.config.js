093bbdd0cb4894b6fe8c81ec49c88606aff9e064
brother1-4752 <guddlf4752@gmail.com>
2023-09-20 07:38:25 +0900

    style: prettier에 따른 스타일 적용 / 레이아웃 컴포넌트 위치 변경, chore: 초기 경로 설정, 리액트 훅 폼 패키치 설치

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/signin',
+        permanent: false,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Global, ThemeProvider } from '@emotion/react'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Global, ThemeProvider } from '@emotion/react';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-import globalStyles from '@/styles/globalStyles'
-import Layout from '@/components/Layout/Layout'
-import theme from '@/styles/theme'
+import globalStyles from '@/styles/globalStyles';
+import theme from '@/styles/theme';
+import Layout from '@/components/Common/Layout/Layout';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -18,11 +18,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
           <Global styles={globalStyles} />
+
           <Layout>
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
       </QueryClientProvider>
     </>
-  )
+  );
 }
