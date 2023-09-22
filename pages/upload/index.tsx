40b0116807e9a1e2334fc6aa206b27c433f6fd58
brother1-4752 <guddlf4752@gmail.com>
2023-09-22 18:45:38 +0900

    feat: 업로드 화면 기본 요소 구현(stt, 감정분석, 유튜브 bgm기능 제외)

--- pages/upload/index.tsx
+++ pages/upload/index.tsx
@@ -1,5 +1,55 @@
+import { CreateStt } from '@/hooks/useCreateStt';
+import axios from 'axios';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+export type UploadInputProps = {
+  selectedOption: string;
+  video: string;
+  audio: string;
+  text: string;
+  isAlramed: boolean;
+  isRepresentative: boolean;
+};
+
 const Upload = () => {
-  return <div>Upload</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<UploadInputProps>();
+
+  const onSubmitHandler: SubmitHandler<UploadInputProps> = async (data) => {
+    // const testData = await CreateStt(data.audio[0]);
+    // console.log(testData);
+    console.log(data.audio[0]);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
+        <select {...register('selectedOption')}>
+          <option value="Video">Video</option>
+          <option value="Audio">Audio</option>
+          <option value="Text">Text</option>
+        </select>
+
+        {/* 선택한 옵션에 따라 input를 상황에 맞게 보여준다. */}
+        {/* TODO: 겹치지 않도록, 재사용성 높이기 */}
+
+        {watch('selectedOption') === 'Video' && (
+          <input {...register('video')} type="file" accept="video/*"></input>
+        )}
+        {watch('selectedOption') === 'Audio' && (
+          <input {...register('audio')} type="file" accept="audio/*"></input>
+        )}
+        {watch('selectedOption') === 'Text' && <input {...register('text')} />}
+
+        {/* naver STT 세팅되면, 텍스트화하고 감정분석 유틸함수로 프로퍼티 넘겨주기 */}
+        <button>제출</button>
+      </form>
+    </div>
+  );
 };
 
 export default Upload;
